e492ca05f78e8afd7796ada6a558115bf4e04671
XieHangX <xiehang2005@gmail.com>
2020-10-23 17:37:01 +0800

    Update GeoScene_Sentinel1.py

--- GeoScene-Sentinel1/GeoScene_Sentinel1.py
+++ GeoScene-Sentinel1/GeoScene_Sentinel1.py
@@ -41,16 +41,27 @@ class RasterTypeFactory():
         #sys_sensorname	传感器名称	Text	True			255
         #sys_productname	产品名称	Text	True			255
         self.sensorName_auxField = arcpy.Field()
-        self.sensorName_auxField.name = 'sensorname'
+        self.sensorName_auxField.name = 'SensorName'
         self.sensorName_auxField.aliasName = 'Sensor Name'
         self.sensorName_auxField.type = 'String'
         self.sensorName_auxField.length = 50
 
-        self.acquisitiondate_auxField = arcpy.Field()
-        self.acquisitiondate_auxField.name = 'acquisitiondate'
-        self.acquisitiondate_auxField.aliasName = 'Acquisition Date'
-        self.acquisitiondate_auxField.type = 'date'
-
+        self.acquisitionDate_auxField = arcpy.Field()
+        self.acquisitionDate_auxField.name = 'AcquisitionDate'
+        self.acquisitionDate_auxField.aliasName = 'Acquisition Date'
+        self.acquisitionDate_auxField.type = 'date'
+        
+        self.dirName_auxField = arcpy.Field()
+        self.dirName_auxField.name = 'DirName'
+        self.dirName_auxField.aliasName = 'Dir Name'
+        self.dirName_auxField.type = 'String'
+        self.dirName_auxField.length = 2048
+        
+        #self.previewImage_auxField = arcpy.Field()
+        #self.previewImage_auxField.name = 'PreviewImage'
+        #self.previewImage_auxField.aliasName = 'Preview Image'
+        #self.previewImage_auxField.type = 'Blob'
+        
 
         return [
                 {
@@ -87,27 +98,30 @@ class RasterTypeFactory():
                     #        'functionTemplate': 'DataCube_S1_SAR.rft.xml'
                     #    }
                     #],
-                    # GET THE CORRECT BAND INDEX , MIN MAX WAVELENGTH
-                    'bandProperties': [
-                        {
-                            'bandName': 'vh',
-                            'bandIndex': 1,
-                            'wavelengthMin': 180000000.0,  # C band with central frequency of 5.405 GHz
-                            'wavelengthMax': 180000000.0,
-                            'datasetTag': 'SAR'
-                        },
-                        {
-                            'bandName': 'vv',
-                            'bandIndex': 2,
-                            'wavelengthMin': 180000000.0,  # C band with central frequency of 5.405 GHz
-                            'wavelengthMax': 180000000.0,
-                            'datasetTag': 'SAR'
-                        }
-                    ],
+                    ## GET THE CORRECT BAND INDEX , MIN MAX WAVELENGTH
+                    #'bandProperties': [
+                    #    {
+                    #        'bandName': 'vh',
+                    #        'bandIndex': 1,
+                    #        'wavelengthMin': 180000000.0,  # C band with central frequency of 5.405 GHz
+                    #        'wavelengthMax': 180000000.0,
+                    #        'datasetTag': 'SAR'
+                    #    }
+                    #    ,
+                    #    {
+                    #        'bandName': 'vv',
+                    #        'bandIndex': 2,
+                    #        'wavelengthMin': 180000000.0,  # C band with central frequency of 5.405 GHz
+                    #        'wavelengthMax': 180000000.0,
+                    #        'datasetTag': 'SAR'
+                    #    }
+                    #],
                     # GET THE CORRECT BAND INDEX , MIN MAX WAVELENGTH
 
                     'fields': [self.sensorName_auxField,
-                               self.acquisitiondate_auxField]
+                               self.acquisitionDate_auxField,
+                               self.dirName_auxField]
+                                #,self.previewImage_auxField
                 }
                ]
 
@@ -142,7 +156,7 @@ class GeoSceneSentinelBuilder():
             ##rasterInfo['nCols'] = nCols
             ##rasterInfo['nRows'] = nRows
             ##rasterInfo['nBands'] = noBands
-            #rasterInfo['spatialReference'] = 4326
+            #rasterInfo['spatialReference'] = int('4326')
             #rasterInfo['XMin'] = 111.711006
             #rasterInfo['YMin'] = 23.911518
             #rasterInfo['XMax'] = 114.473335
@@ -153,20 +167,33 @@ class GeoSceneSentinelBuilder():
             builtItem['itemUri'] = itemURI
 
             vertex_array = arcpy.Array()
-            all_vertex = '23.911518,111.711006 24.327019,114.147614 22.646614,114.473335 22.227821,112.068756'.split(" ")
-            if all_vertex is not None:
+            footprintCoords = self.utilities.getFootPrintCoords(path)
+            if footprintCoords is not None:
+                all_vertex = footprintCoords.split(" ")
                 for vertex in all_vertex:
                     point = vertex.split(',')
                     vertex_array.add(arcpy.Point(float(point[1]), float(point[0])))
             footprint_geometry = arcpy.Polygon(vertex_array)
-            # builtItem['footprint'] = footprint_geometry
-
+            builtItem['footprint'] = footprint_geometry
             
-            builtItem['spatialReference'] = int('4326')
+            sr = self.utilities.getFootPrintSR(path)
+            builtItem['spatialReference'] = int(sr)
             
             metadata = {}
-            metadata['sensorname'] = self.utilities.getSensorName(path)
-            metadata['acquisitiondate'] = '2020-10-21'
+            metadata['SensorName'] = self.utilities.getSensorName(path)
+            metadata['AcquisitionDate'] = self.utilities.getAcquistionDate(path)
+            metadata['DirName'] = os.path.dirname(path)
+            
+            #quicklookpath = self.utilities.getQuickLook(path)
+            #f = open(os.path.abspath(quicklookpath), "rb")
+            #img = f.read()
+            #f.close()
+            #metadata['previewImage'] = str(img)
+
+            # str.encode("UTF-8")
+            # img.decode('UTF-8','strict'))
+            # metadata['previewImage'] = img.decode('UTF-8','strict')
+            # mdb.escape_string(img)
 
             builtItem['keyProperties'] = metadata
 
@@ -246,6 +273,13 @@ class GeoSceneSentinelCrawler():
         return uri
 
 class Utilities():
+
+    #__namespace : None
+    #__tree: None
+
+    def __init__(self):
+        self.__tree = None
+        self.__namespace = None
     
     def isTarget(self, path):
 
@@ -261,6 +295,11 @@ class Utilities():
             return 'SAR'
         return None
     
+    def getTree(self,path):
+        if self.__tree is None:
+            self.__tree = cacheElementTree(path)
+        return self.__tree
+
     def __getMetadataObjectByID(self,tree,id):
         
         if tree is not None:
@@ -272,7 +311,7 @@ class Utilities():
                         return child
         return None
 
-    def __getDataObjectByID(self,tree,id):
+    def getDataObjectByID(self,tree,id):
         
         if tree is not None:
             root = tree.getroot()
@@ -284,9 +323,9 @@ class Utilities():
 
     def getQuickLook(self,path):
         
-        tree = cacheElementTree(path)
+        tree = self.getTree(path)
         if tree is not None:
-            quicklookNode = self.__getDataObjectByID(tree,'quicklook')
+            quicklookNode = self.getDataObjectByID(tree,'quicklook')
             if quicklookNode is not None:
                 href = quicklookNode.find('byteStream/fileLocation').get('href')                
                 #os.chdir(path)
@@ -296,15 +335,36 @@ class Utilities():
         return None
     
     def getNamespace(self,path):
-        namespaces = dict([
-            node for _, node in ET.iterparse(
-                path, events=['start-ns']
-            )
-        ])
-        return namespaces
+        if self.__namespace is None:
+            self.__namespace = dict([node for _, node in ET.iterparse(path, events=['start-ns'])])
+        return self.__namespace
+    
+    def getFootPrintCoords(self, path):
+        tree = self.getTree(path)
+        if tree is not None:
+            metadataObjectNode = self.__getMetadataObjectByID(tree,'measurementFrameSet')
+            namespaces = self.getNamespace(path)
+            if metadataObjectNode is not None:
+                xmlData = metadataObjectNode.find('metadataWrap/xmlData')
+                coords = xmlData.find('safe:frameSet/safe:frame/safe:footPrint/gml:coordinates',namespaces)
+                if coords is not None:
+                    return coords.text
+        return None
 
+    def getFootPrintSR(self, path):
+        tree = self.getTree(path)
+        if tree is not None:
+            metadataObjectNode = self.__getMetadataObjectByID(tree,'measurementFrameSet')
+            namespaces = self.getNamespace(path)
+            if metadataObjectNode is not None:
+                xmlData = metadataObjectNode.find('metadataWrap/xmlData')
+                footprint = xmlData.find('safe:frameSet/safe:frame/safe:footPrint',namespaces)
+                if footprint is not None:
+                    return footprint.get('srsName').split('#')[1]
+        return None
+    
     def getSensorName(self, path):
-        tree = cacheElementTree(path)
+        tree = self.getTree(path)
         if tree is not None:
             metadataObjectNode = self.__getMetadataObjectByID(tree,'platform')
             namespaces = self.getNamespace(path)
@@ -317,13 +377,25 @@ class Utilities():
                     return sensorName
         return None
 
+    def getAcquistionDate(self, path):
+        tree = self.getTree(path)
+        if tree is not None:
+            metadataObjectNode = self.__getMetadataObjectByID(tree,'acquisitionPeriod')
+            namespaces = self.getNamespace(path)
+            if metadataObjectNode is not None:
+                xmlData = metadataObjectNode.find('metadataWrap/xmlData')
+                acquisitionPeriod = xmlData.find('safe:acquisitionPeriod/safe:startTime',namespaces)
+                if acquisitionPeriod is not None:
+                    return acquisitionPeriod.text[0:19].replace("T", " ")
+        return None
+
     def getProductName(self, path):
         """        
         get Product Type
         :param path: manifest.safe
         :return:
         """
-        tree = cacheElementTree(path)
+        tree = self.getTree(path)
         if tree is not None:
             metadataObjectNode = self.__getMetadataObjectByID(tree,'generalProductInformation')
             namespaces = self.getNamespace(path)
